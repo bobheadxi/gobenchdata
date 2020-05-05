a0915768a11dd7dd7b253ea8b98c3567d2c88db1
Robert Lin <robert@bobheadxi.dev>
2020-05-05 10:43:04 -0700

    web: add place to put css overrides

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  // serve on any path - single-page app with no history, so we should be fine
   publicPath: './',
+
+  chainWebpack: config => {
+    // make it easier to adjust output
+    config.plugin('html')
+      .tap(args => {
+        args[0].minify = false;
+        return args;
+      });
+  },
 };
