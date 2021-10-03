34ff2ddbe5ef9eb02810d2cafd970460dccdb286
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2021-10-03 23:35:36 +0300

    Update parser.go

--- bench/parser.go
+++ bench/parser.go
@@ -98,7 +98,7 @@ func (p *Parser) readBenchmark(line string) (*Benchmark, error) {
 
 	// runs - doesn't include units
 	tmp, split = internal.Popleft(split)
-	if bench.Runs, err = strconv.Atoi(tmp); err != nil && !strings.Contains("Intel(R)", line) {
+	if bench.Runs, err = strconv.Atoi(tmp); err != nil && !strings.Contains(line, "Intel(R)") {
 		return nil, fmt.Errorf("%s: could not parse run: %w (line: %s)", bench.Name, err, line)
 	}
 
